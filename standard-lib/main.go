bcf3c5a3f46ac23eb9662c94164aabd96154823b
zhengyongzhen <zheng_yzh@163.com>
2024-04-18 09:51:31 +0800

    正则

---
+++ standard-lib/main.go
@@ -0,0 +1,14 @@
+package main
+
+import _case "CloudNative/standard-lib/case"
+
+func main() {
+	_case.EncodingCase()
+	_case.ErrorsCase()
+	_case.FmtCase()
+	_case.FmtCase1()
+	_case.LogCase()
+	_case.MathCase()
+	_case.ReflectCase()
+	_case.RegexpCase()
+}
