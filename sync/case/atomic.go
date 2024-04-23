63fdfc96236333ed4197d1c4d71b3d068bbb5cc6
zhengyongzhen <zheng_yzh@163.com>
2024-04-24 00:40:49 +0800

    并发编程-atomic通过内存共享数据

---
+++ sync/case/atomic.go
@@ -0,0 +1,90 @@
+package _case
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+func AtomicCase() {
+	var count int64 = 5
+	atomic.StoreInt64(&count, 10)
+	fmt.Println("获取变量的值 ", atomic.LoadInt64(&count))
+
+	//count += 10
+	fmt.Println("原有基础上累加后变量的值 ", atomic.AddInt64(&count, 10))
+
+	fmt.Println("交换变量的值，并返回原有的值:  ", atomic.SwapInt64(&count, 30))
+	fmt.Println("交换之后，获取变量的值 ", atomic.LoadInt64(&count))
+
+	// 比较并替换原有的值，并返回是否替换成功。通过旧值对比，如果相等，则进行替换
+	fmt.Println("比较并替换原有的值，并返回是否替换成功:  ", atomic.CompareAndSwapInt64(&count, 30, 40))
+	fmt.Println("比较替换之后，获取变量的值 ", atomic.LoadInt64(&count))
+
+	// 通过旧值对比，如果不等，则不进行替换
+	fmt.Println("比较并替换原有的值，并返回是否替换成功:  ", atomic.CompareAndSwapInt64(&count, 30, 100))
+	fmt.Println("比较替换之后，获取变量的值 ", atomic.LoadInt64(&count))
+}
+
+type atomicCounter struct {
+	count int64
+}
+
+func (c *atomicCounter) Inc() {
+	atomic.AddInt64(&c.count, 1)
+}
+
+func (c *atomicCounter) Load() int64 {
+	return atomic.LoadInt64(&c.count)
+}
+
+func AtomicCase1() {
+	var count = atomicCounter{}
+	wg := sync.WaitGroup{}
+	for i := 0; i < 20000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			count.Inc()
+
+		}()
+	}
+	wg.Wait()
+	fmt.Println(count.Load())
+}
+
+func AtomicCase2() {
+	list := []string{"A", "B", "C", "D"}
+	// 定义一个原子值
+	var atomicMp atomic.Value
+	// 定义一个集合
+	mp := map[string]int{}
+	// 涉及到compareAndSwap,所以使用地址
+	atomicMp.Store(&mp)
+	wg := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+		atomicLabel:
+			m := atomicMp.Load().(*map[string]int)
+			m1 := map[string]int{}
+			for k, v := range *m {
+				m1[k] = v
+			}
+			for _, item := range list {
+				_, ok := m1[item]
+				if !ok {
+					m1[item] = 0
+				}
+				m1[item]++
+			}
+			success := atomicMp.CompareAndSwap(m, &m1)
+			if !success {
+				goto atomicLabel
+			}
+		}()
+	}
+	wg.Wait()
+	fmt.Println(atomicMp.Load())
+}
